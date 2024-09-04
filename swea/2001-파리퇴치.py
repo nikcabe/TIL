481c062c45a7461db52d0a8cb11e0a36e4c26565
seo <jjkkgh123@naver.com>
2024-09-04 17:50:48 +0900

    swea

--- swea/2001-파리퇴치.py
+++ swea/2001-파리퇴치.py
@@ -1,16 +1,15 @@
 T = int(input())
 
 for tc in range(1,1+T):
-    n, m = map(int, input().split())
+    n, m = map(int,input().split())
     arr = [list(map(int,input().split())) for _ in range(n)]
-    # 가장 큰 값 설정
-    max_n = 0
+    max_num = 0
     for i in range(n-m+1):
         for j in range(n-m+1):
-            sum_m = 0
+            sum_num = 0
             for k in range(m):
                 for l in range(m):
-                    sum_m += arr[i+k][j+l]
-            if max_n < sum_m:
-                max_n = sum_m
-    print(max_n)
+                    sum_num += arr[i+k][j+l]
+            if max_num < sum_num:
+                max_num = sum_num
+    print(max_num)
--- swea/4836-색칠하기.py
+++ swea/4836-색칠하기.py
@@ -2,13 +2,13 @@ T = int(input())
 
 for tc in range(1,1+T):
     n = int(input())
+    arr = [[0] * 10 for _ in range(10)]
     cnt = 0
-    empty = [[0]*10 for _ in range(10)]
-    for _ in range(n):
-        a1, b1, a2, b2, c = list(map(int, input().split()))
+    for k in range(n):
+        a1,b1,a2,b2,c = list(map(int,input().split()))
         for i in range(a1,a2+1):
             for j in range(b1,b2+1):
-                empty[i][j] += c
-                if empty[i][j] == 3:
+                arr[i][j] += c
+                if arr[i][j] == 3:
                     cnt += 1
     print(f'#{tc} {cnt}')
