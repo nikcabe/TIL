c9159024516a08f60a1f978a3f4ae01993e4a604
Seo KeonHo <jjkkgh123@naver.com>
2024-09-25 02:07:40 +0900

    baekjoon

---
+++ baekjoon/17142-연구소3.py
@@ -0,0 +1,55 @@
+from itertools import combinations
+from collections import deque
+
+n, m = map(int,input().split())
+board = []
+virus = []
+# 바이러스 넣을 수 있는 곳 전부 확인
+for i in range(n):
+    board.append(list(map(int,input().split())))
+    for j in range(n):
+        if board[i][j] == 2:
+            virus.append((i,j))
+def bfs(virus):
+    # 바이러스가 가장 오래 걸린 곳의 시간을 파악 하기 위해 max_time을 사용함
+    max_time = 0
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    # 방문 했는지 여부를 파악하기 위해 -1의 NxN 배열을 만듬
+    visited = [[-1] * n for _ in range(n)]
+    # 조합에서 뽑힌 바이러스의 자리에서 시작하기 때문에 방문표시 해줌
+    for v in virus:
+        visited[v[0]][v[1]] = 0
+    q = deque(virus)
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == -1:
+                # 방문 한적 없고 벽이 아닌 곳일 경우 q에 추가
+                if board[nx][ny] == 0:
+                    q.append((nx, ny))
+                    # 1번 갈때 1초 증가하기 때문에 +1을 해줌
+                    visited[nx][ny] = visited[x][y] + 1
+                    max_time = max(max_time, visited[nx][ny])
+                elif board[nx][ny] == 2:
+                    visited[nx][ny] = visited[x][y] + 1
+    # 다 돌렸으나 감염시키지 못했거나 방문하지 않았다면 원래 값을 돌려줌
+    for i in range(n):
+        for j in range(n):
+            if (board[i][j] == 0 or board[i][j] == 2) and visited[i][j] == -1:
+                return 1e9
+
+    return max_time
+ans = 1e9
+# combinations를 사용하요 조합을 사용
+for comb in combinations(virus,m):
+    # bfs돌린 것을 하나하나 비교해줌
+    ans = min(bfs(comb),ans)
+# 값이 변화했다면
+if ans != 1e9:
+    print(ans)
+# 값이 그대로라면
+else:
+    print(-1)
---
+++ baekjoon/3190-뱀.py
@@ -0,0 +1,59 @@
+n = int(input())
+k = int(input())
+
+graph = [[0] * n for _ in range(n)]
+
+d = 0 # 0 : D, 1: L
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for _ in range(k):
+    r, c = map(int, input().split())
+    graph[r-1][c-1] = 2
+
+l = int(input())
+turn_info = []
+for _ in range(l):
+    t, c = input().split()
+    turn_info.append((int(t), c))
+
+def rotate(c):
+    global d
+    if c == 'D':
+        d = (d + 1) % 4
+    else:
+        d = (d - 1) % 4
+count = 0
+turn_index = 0
+sx, sy = 0, 0
+graph[0][0] = 1
+snake = []
+snake.append((0, 0))
+
+while True:
+    count += 1
+    nx = sx + dx[d]
+    ny = sy + dy[d]
+    if 0 <= nx < n and 0 <= ny < n:
+        if graph[nx][ny] == 2:
+            snake.append((nx, ny))
+            graph[nx][ny] = 1
+            sx, sy = nx, ny
+        elif graph[nx][ny] == 1:
+            break
+        elif graph[nx][ny] == 0:
+            tx, ty = snake.pop(0)
+            graph[tx][ty] = 0
+            snake.append((nx, ny))
+            graph[nx][ny] = 1
+            sx, sy = nx, ny
+    else:
+        break
+        # 회전 시키기
+    if turn_index < l:
+        if count == turn_info[turn_index][0]:
+            rotate(turn_info[turn_index][1])
+            turn_index += 1
+
+
+print(count)
